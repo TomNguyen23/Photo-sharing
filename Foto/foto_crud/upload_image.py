2c26b767579403312b1feba90dfd4d2620da0c6d
Makememyday <congthinhluong.new@gmail.com>
2024-05-15 13:58:11 +0700

    feat: continuous crud

--- Foto/foto_crud/upload_image.py
+++ Foto/foto_crud/upload_image.py
@@ -11,9 +11,23 @@ class ImgurUpload:
             data = file.read()
             base64_data = base64.b64encode(data)
 
-        response = requests.post(self.url, headers=self.headers, data={"image": base64_data})
-        url = response.json()["data"]["link"]
-        return url
+        # response = requests.post(self.url, headers=self.headers, data={"image": base64_data}, verify=False)
+        payload={'type': 'image',
+            'title': 'Simple upload',
+            'description': 'This is a simple image upload in Imgur'
+        }
+        files=[
+            ('image',('ArtPTXoAAvuny.jpeg',open(image_path,'rb'),'image/jpeg'))
+        ]
+        headers = {
+        'Authorization': 'Client-ID fbe9deb628a0276'
+        }
+
+        response = requests.request("POST", self.url, headers=headers, data=payload, files=files)
+
+
+        # url = response.json()["data"]["link"]
+        return response.json()
     
     def upload_image_from_file(self, file):
         data = file.read()
@@ -26,3 +40,18 @@ class ImgurUpload:
         response = requests.post(self.url, headers=self.headers, data={"image": base64_data})
         url = response.json()["data"]["link"]
         return url
+    
+imgur = ImgurUpload()
+
+# Upload all file in Daniel folder, write the result in Daniel/Daniel.txt
+import os
+path = "Daniel"
+files = os.listdir(path)
+with open("Daniel/Daniel.txt", "w") as file:
+    for f in files:
+        try: 
+            file.write(f + " : " + imgur.upload_image_from_image_path(path + "/" + f)['data']['link'] + "\n")
+        except:
+            continue
+
+
